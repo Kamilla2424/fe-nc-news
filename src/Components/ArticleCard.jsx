a432c303348b08d494a3f55d2e391606a696b119
Kamilla2424 <mohamedkamilla6@gmail.com>
2024-03-12 09:44:23 +0000

    change img size for mobile

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -4,7 +4,7 @@ const ArticleCard = ({article}) => {
     return (
         <Link to={`/articles/${article.article_id}`}>
             <h4>{article.title}</h4>
-            <img src={article.article_img_url}/>
+            <img src={article.article_img_url} width={500}/>
             <p>{article.author}</p>
             <p>{article.created_at}</p>
             <p>{article.votes}</p>
