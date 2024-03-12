e2cce240a8e02c8fbd05d7f2f5b79a3a8217ede4
Kamilla2424 <mohamedkamilla6@gmail.com>
2024-03-12 14:11:04 +0000

    view-all-comments-for-article complete

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { fetchArticleById } from "../../utils"
+import { fetchArticleById, fetchCommentsById } from "../../utils"
 
 const Article = () => {
     const {article_id} = useParams()
     const [article, setArticle] = useState({})
+    const [comments, setComments] = useState([])
 
     useEffect(() => {
         fetchArticleById(article_id).then(({article}) => {
             setArticle(article)
+        })
+        fetchCommentsById(article_id).then(({comments}) => {
+            setComments(comments)
         })
     }, [])
 
@@ -18,7 +22,21 @@ const Article = () => {
         <h3>{article.author}</h3>
         <img src={article.article_img_url} width={500}/>
         <p>{article.body}</p>
-        <p>{article.votes}</p>
+        <p>Votes: {article.votes}</p>
+        <h3 className="comment-header">Comments:</h3>
+        <div className="comment-section">
+        {comments.map((comment) => {
+            return (
+            <ul className="comment-list">
+            <li className="comment" key={comment.comment_id}>
+                <p className="comment-author">@{comment.author}</p>
+                <p className="comment-body">{comment.body}</p>
+                <p className="comment-date">{comment.created_at}</p>
+            </li>
+            </ul>
+            )
+        })}
+        </div>
         </>
     )
 }
